bbfb25b7171d2af47c1ae798a29386aeca5232e9
Augs Machado <augustomachadoramos@gmail.com>
2019-07-25 18:20:25 -0300

    Create 2602.sql

---
+++ 2602.sql
@@ -0,0 +1,35 @@
+/*
+  Name: Problem 2602
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/25/2019  6:19 pm
+	Comment language: en
+	Description: 
+    Your company is doing a survey of how many customers are registered in the states, however,
+      lacked to raise the data of the state of the 'Rio Grande do Sul'.
+    Then, you must show the names of all customers whose state is 'RS'.
+*/
+ 
+CREATE TABLE customers (
+  id NUMERIC PRIMARY KEY,
+  name CHARACTER VARYING (255),
+  street CHARACTER VARYING (255),
+  city CHARACTER VARYING (255),
+  state CHAR (2),
+  credit_limit NUMERIC
+);
+
+INSERT INTO customers (id, name, street, city, state, credit_limit)
+VALUES 
+  (1,	'Pedro Augusto da Rocha',	'Rua Pedro Carlos Hoffman',	'Porto Alegre',	'RS',	700.00),
+  (2,	'Antonio Carlos Mamel',	'Av. Pinheiros', 'Belo Horizonte',	'MG',	3500.50),
+  (3,	'Luiza Augusta Mhor',	'Rua Salto Grande',	'Niteroi',	'RJ',	4000.00),	
+  (4,	'Jane Ester',	'Av 7 de setembro',	'Erechim',	'RS',	800.00),
+  (5, 'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
+
+  
+  /* Solution */
+  SELECT name FROM customers WHERE state="RS";
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE customers; --
