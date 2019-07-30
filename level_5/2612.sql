f1bc7788bbcd5e7a2673f08b139562d8a75bd2b0
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:39:09 -0300

    Create 2612.sql

---
+++ level_5/2612.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2612
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Once actors win big awards and recognition in the film world, the demand for films in which they
+			participate increases. So we want to know what movies the brothers Silva have in our catalog.
+		To do this, select the code and the name of the movies in which the actors 'Marcelo Silva' or 'Miguel Silva'
+			acted and that the genre of the film is 'Action'.
+*/
