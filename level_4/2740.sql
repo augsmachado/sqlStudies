e358ceb99dfc0be50b83bb7cb2394442dda60438
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:37:28 -0300

    Create 2740.sql

---
+++ level_4/2740.sql
@@ -0,0 +1,13 @@
+/*
+  Name: Problem 2740
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The International Underground Excavation League is a success between alternative sports,
+			however the staff responsible for organizing the events doesn’t understand computers at all,
+			they only know how to dig and the sport rule set. As such, you were hired to solve the League’s problem.
+		Select the three first placed with the initial phrase "Podium: " and select the last two, which will be
+			demoted to a lower league with the initial phrase “Demoted:".
+*/
