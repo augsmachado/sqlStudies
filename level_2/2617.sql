85e4805b6cd2be64c64f673ad30382c774599c93
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:11:52 -0300

    Create 2617.sql

---
+++ level_2/2617.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2617
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The financial sector has encountered some problems in the delivery of one of our providers,
+			the delivery of the products does not match the invoice.
+		Your job is to display the name of the products and the name of the provider, for the products supplied by
+			the provider 'Ajax SA'.
+*/
