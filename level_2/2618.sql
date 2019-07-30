3f0a34264f393799f5defd2c2d24a90b8d8398af
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:14:17 -0300

    Create 2618.sql

---
+++ level_2/2618.sql
@@ -0,0 +1,11 @@
+/*
+  Name: Problem 2618
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Our company's import sector needs a report on the import of products from our Sansul providers.
+		Your task is to display the name of the products, the name of the supplier and the name of the category,
+			for the products supplied by the supplier 'Sansul SA' and whose category name is 'Imported'.
+*/
