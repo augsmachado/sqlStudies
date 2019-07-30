3ee4f265bb3b2d69cd98a2bd6376d430125c03e1
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:07:41 -0300

    Create 2746.sql

---
+++ level_1/2746.sql
@@ -0,0 +1,13 @@
+/*
+  Name: Problem 2746
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Viruses are evolving, but new research has proven that by switching some proteins the vaccine becomes unbeatable.
+			The protein H1(Hemagglutinin) when replaced by the X protein (Xenomorphina) has interesting effects against almost
+			every viral disease. Some conspiracists say that after the vaccine’s discovery some strange 3 meters tall
+			creatures were found in the surroundings of the laboratories, but this is clearly a lie.
+		Therefore, you must replace every string “H1”( Hemagglutinin ) by 'X' ( Xenomorphina ).
+*/
