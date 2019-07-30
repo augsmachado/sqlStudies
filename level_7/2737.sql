4f9129ec9e50bea2470e72e756dbca4332c61f9d
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:51:51 -0300

    Create 2737.sql

---
+++ level_7/2737.sql
@@ -0,0 +1,13 @@
+/*
+  Name: Problem 2737
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The manager of Mangojata Lawyers requested a report on the current lawyers.
+		The manager wants you to show him the name of the lawyer with the most clients, the one with the
+			fewest and the client average considering all lawyers.
+		OBS: Before presenting the average, show a field called Average to make the report more readable.
+			The average must be presented as an integer.
+*/
