46b667f5850f63ea5a282941f896edeeabc5ad5b
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:16:13 -0300

    Create 2621.sql

---
+++ level_2/2621.sql
@@ -0,0 +1,13 @@
+/*
+  Name: Problem 2621
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		When it comes to delivering the report on how many products the company has in stock,
+			a part of the report has become corrupted, so the stock keeper asked for help, he wants you to display the
+			following data for him.
+		Display the name of products whose amount are between 10 and 20 and whose name of the supplier starts with the
+			letter 'P'.
+*/
