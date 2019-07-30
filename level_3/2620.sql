45e2c3a21bb959b63fa2c33548e47ebdec9200f8
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:27:32 -0300

    Create 2620.sql

---
+++ level_3/2620.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2620
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The company's financial audit is asking us for a report for the first half of 2016.
+			Then display the customers name and order number for customers who placed orders in the first half of 2016.
+*/
