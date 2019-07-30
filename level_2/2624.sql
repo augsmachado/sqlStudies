10b61a5e4324d5ea7d9f123746b00b53b7ee9ac2
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:17:00 -0300

    Create 2624.sql

---
+++ level_2/2624.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2624
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The company board asked you for a simple report on how many cities the company has already reached.
+		To do this you must display the number of distinct cities in the customers table.
+*/
