bbe78071f13db664a2d719bd2b055a5759233cbc
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:25:19 -0300

    Create 2610.sql

---
+++ level_3/2610.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2610
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		In the company that you work is being done a survey on the values of the products that are marketed.
+		To help the industry that is doing this survey you should calculate and display the average value of
+			the price of the products.
+		OBS: Show the value with two numbers after the period.
+*/
