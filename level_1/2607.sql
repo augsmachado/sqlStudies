bc8041c2fbc571824b2f65f8168a2992fe3d33d3
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 16:56:47 -0300

    Create 2607.sql

---
+++ level_1/2607.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2607
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Every month the company asks for a report from the cities that providers are registered.
+			So, do a query that returns all the cities of the providers, but in alphabetical order.
+			
+		OBS: You must not show repeated cities.
+*/
