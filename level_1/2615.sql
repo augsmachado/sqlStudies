9edd0a9538c6233e418f013ce3abb959b7a46180
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:04:01 -0300

    Create 2615.sql

---
+++ level_1/2615.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2615
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The video store company has the objectives of creating several franchises spread throughout Brazil.
+			For this we want to know in which cities our customers live.
+		For you to help us select the name of all the cities where the rental company has clients.
+			But please do not repeat the name of the city.
+*/
