ceb1354b2e63a4b4a1b85a73337fe764d2a505bb
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:06:18 -0300

    Create 2744.sql

---
+++ level_1/2744.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2744
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		You were hired to be the consultant for a company. Analyzing the database, you noticed that the passwords are
+			stored as text files and, as everyone knows, this is a terrible security practice as they are not encrypted.
+		Therefore you must convert every password to the MD5 format. Show the client id, the password before conversion
+			and the new MD5.
+*/
