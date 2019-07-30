3436e09728bf0399fe6984934a3133ff35ae9fc1
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:41:36 -0300

    Create 2738.sql

---
+++ level_5/2738.sql
@@ -0,0 +1,15 @@
+/*
+  Name: Problem 2738
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The Mars Technology University has Open Positions for researchers. However, the computer responsible for
+			processing the candidates' data is broken. You must present the candidate list, containing the name and
+			final score (with two decimal places of precision) of each candidate. Remember to show the list ordered
+			by score (highest first).
+		The score is given by the weighted average described as:
+		
+		Avg = (math * 2) + (specific * 3) + (project_plan * 5) / 10
+*/
