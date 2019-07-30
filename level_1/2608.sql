ba72d6efe394c7bb85b4926e6df7a9099055859d
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 16:59:57 -0300

    Create 2608.sql

---
+++ level_1/2608.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2608
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description: 
+    	The financial sector of our company, wants to know the smaller and higher values of the products, which we sell.
+      	For this you must display only the highest and lowest price of the products table.
+*/
