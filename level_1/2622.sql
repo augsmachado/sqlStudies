d11bb4eabba55ec999e1c301c805ac2713ef0822
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:05:04 -0300

    Create 2622.sql

---
+++ level_1/2622.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2622
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The sales industry wants to make a promotion for all clients that are legal entities.
+			For this you must display the name of the customers that are legal entity.
+*/
