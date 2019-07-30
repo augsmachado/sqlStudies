71d0ae3e3a6ffe05df272551385896ad5e07c9b6
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:36:32 -0300

    Create 2739.sql

---
+++ level_4/2739.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2739
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The Central Bank of Financing lost many registers after a server failure that happened last October.
+			The collection dates for the parcels where lost.
+		The bank requested your help to select the names and day of month in which each client must pay theirs parcel.
+		OBS: The day of month must be an integer.
+*/
