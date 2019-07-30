d409fdc1d7644a43253bc01e1cabd4a4749509a3
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 18:44:05 -0300

    Update 2615.sql

--- level_1/2615.sql
+++ level_1/2615.sql
@@ -10,3 +10,26 @@
 		For you to help us select the name of all the cities where the rental company has clients.
 			But please do not repeat the name of the city.
 */
+
+CREATE TABLE customers (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  street varchar(50),
+  city varchar(50)
+);
+
+
+INSERT INTO customers (id, name, street, city)
+VALUES
+  (1,	'Giovanna Goncalves Oliveira',	'Rua Mato Grosso',	'Canoas'),
+  (2, 'Kauã Azevedo Ribeiro',	'Travessa Ibiá',	'Uberlândia'),
+  (3,	'Rebeca Barbosa Santos',	'Rua Observatório Meteorológico',	'Salvador'),
+  (4,	'Sarah Carvalho Correia',	'Rua Antônio Carlos da Silva',	'Uberlândia'),
+  (5,	'João Almeida Lima',	'Rua Rio Taiuva',	'Ponta Grossa'),
+  (6,	'Diogo Melo Dias',	'Rua Duzentos e Cinqüenta',	'Várzea Grande');
+  
+/* Solution */
+SELECT DISTINCT city from customers;
+
+/*  Execute this query to drop the tables */
+-- DROP TABLE customers; --
