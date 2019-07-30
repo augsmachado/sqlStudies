00803f057cfba9cfba73bfcc9ef259e290c2997f
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:33:33 -0300

    Create 2614.sql

---
+++ level_4/2614.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2614
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The video store is making its semi-annual report and needs your help. All you have to do is select the
+			name of the clients and the date of rental, from the rentals made in September 2016.
+*/
