806528ef182fa3aef8db57d7d6c290f36c3d5d93
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:50:18 -0300

    Create 2623.sql

---
+++ level_6/2623.sql
@@ -0,0 +1,11 @@
+/*
+  Name: Problem 2623
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The sales industry needs a report to know what products are left in stock.
+		To help the sales industry, display the product name and category name for products whose
+			amount is greater than 100 and the categorie ID is 1,2,3,6 or 9.
+*/
