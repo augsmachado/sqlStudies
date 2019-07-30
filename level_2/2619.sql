f0b30c23d15ba462cc95e18788d43516795b2fda
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:15:10 -0300

    Create 2619.sql

---
+++ level_2/2619.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2619
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Our company is looking to make a new contract for the supply of new super luxury products,
+			and for this we need some data of our products.
+		Your job is to display the name of the products, the name of the providers and the price,
+			for the products whose price is greater than 1000 and its category is' Super Luxury.
+*/
