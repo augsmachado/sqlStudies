d1391866dcf8de9618d42716549ae657314b5896
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:26:47 -0300

    Create 2611.sql

---
+++ level_3/2611.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2611
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		A video store contractor hired her services to catalog her movies. They need you to select
+			the code and the name of the movies whose description of the genre is 'Action'.
+*/
