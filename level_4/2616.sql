4faf080083a1738f3e6b2e0e604f39beb0044b96
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:34:36 -0300

    Create 2616.sql

---
+++ level_4/2616.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2616
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		The video store company intends to do a promotion for customers who have not yet done any rental.
+		Your job is to deliver us the ID and the name of the customers who have not done any rental.
+*/
