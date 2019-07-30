8080ffb4bb9edfd7c6b1fee77a911b0b51e8e4d9
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:01:19 -0300

    Update 2608.sql

--- level_1/2608.sql
+++ level_1/2608.sql
@@ -4,7 +4,7 @@
 	Author: Augusto Machado Ramos
 	Date: 07/30/2019
 	Comment language: en
-	Description: 
+	Description:
     	The financial sector of our company, wants to know the smaller and higher values of the products, which we sell.
       	For this you must display only the highest and lowest price of the products table.
 */
