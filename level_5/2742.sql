1f728e22732c83756a526f1b43cfef9276463f48
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:42:51 -0300

    Create 2742.sql

---
+++ level_5/2742.sql
@@ -0,0 +1,14 @@
+/*
+  Name: Problem 2742
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Richard is a famous scientist because of his multiverse theory, where he describes every hypothetical
+			set of parallel universes by means of a database. Thanks to that you now have a job.
+		As your first task, you must select every Richard from dimensions C875 and C774, together with its existence
+			probability (the famous factor N) with three decimal places of precision.
+		Remember that the N factor is calculated by multiplying the omega value by 1,618. The data must be sorted
+			by the least omega value.
+*/
