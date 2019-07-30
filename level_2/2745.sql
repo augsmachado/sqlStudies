84d1e163511710ea959310bb9be967c0428d769d
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:21:35 -0300

    Create 2745.sql

---
+++ level_2/2745.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2745
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		You are going to the International Personal Tax meeting and your proposal is:
+			every individual with income higher than 3000 must pay a tax to the government,
+			which is 10% of his/her income.
+		Show the name and the tax value of each person who earns more than 3000, with two decimal places of precision.
+*/
