18e3b04e4fe005bf59b52a19bbeb7d04fa89a84d
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:32:26 -0300

    Create 2606.sql

---
+++ level_4/2606.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2606
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		When the data were migrated to the database, there was a small misunderstanding on the DBA.
+		Your boss needs you to select the ID and the name of the products, whose categorie name start with 'super'.
+*/
