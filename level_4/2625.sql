b7bc2244cdf2fb3c0785471b752bb5979303d878
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:35:37 -0300

    Create 2625.sql

---
+++ level_4/2625.sql
@@ -0,0 +1,12 @@
+/*
+  Name: Problem 2625
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		Your company's communications managers want a report on the natural person customer data that is
+			registered in the database. But the old report had a problem. customers CPF data came without validation.
+		So your job now is to select all the CPFs of all the customers, and apply a mask on the return of the data.
+		The CPF mask looks like: '000.000.000-00'.
+*/
