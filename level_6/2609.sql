580735c90a8d35ea139c3085a69d4c1e18963178
Augs Machado <augustomachadoramos@gmail.com>
2019-07-30 17:44:46 -0300

    Create 2609.sql

---
+++ level_6/2609.sql
@@ -0,0 +1,10 @@
+/*
+  Name: Problem 2609
+	Copyright: 2019 Augusto Machado Ramos
+	Author: Augusto Machado Ramos
+	Date: 07/30/2019
+	Comment language: en
+	Description:
+		As usual the sales industry is doing an analysis of how many products we have in stock, and you can help them.
+		Then your job will display the name and amount of products of each category.
+*/
