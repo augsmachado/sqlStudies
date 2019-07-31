54b88cc6a3d35ffbedace44917fec61ca1de7908
Augs Machado <augustomachadoramos@gmail.com>
2019-07-31 17:42:51 -0300

    Update 2744.sql

--- level_1/2744.sql
+++ level_1/2744.sql
@@ -10,3 +10,24 @@
 		Therefore you must convert every password to the MD5 format. Show the client id, the password before conversion
 			and the new MD5.
 */
+
+CREATE TABLE account(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  login VARCHAR(255),
+  password VARCHAR(255)
+  );
+  
+  
+INSERT INTO account(id, name, login, password)
+VALUES (1, 'Joyce P. Parry', 'Promeraw', 'noh1Oozei'),
+       (2, 'Michael T. Gonzalez', 'Phers1942', 'Iath3see9bi'),
+       (3, 'Heather W. Lawless', 'Hankicht', 'diShono4'),
+       (4, 'Otis C. Hitt', 'Conalothe', 'zooFohH7w'),
+       (5, 'Roger N. Brownfield', 'Worseente', 'fah7ohNg');
+
+  /* Solution */
+  
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE account; --
