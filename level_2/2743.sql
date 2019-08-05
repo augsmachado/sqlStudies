27afbd92d6f6b3e118674413da9df20bddbf951f
Augs Machado <augustomachadoramos@gmail.com>
2019-08-05 08:50:32 -0300

    Update 2743.sql

--- level_2/2743.sql
+++ level_2/2743.sql
@@ -9,3 +9,23 @@
 			characters people have in theirs names.
 		To help OMCNP, you must show the number of characters of each name sorted by decreasing number of characters.
 */
+
+CREATE TABLE people(
+	id INTEGER PRIMARY KEY,
+	name varchar(255)
+);
+
+
+INSERT INTO people(id, name)
+VALUES 
+      (1, 'Karen'),
+      (2, 'Manuel'),
+      (3, 'Ygor'),
+      (4, 'Valentine'),
+      (5, 'Jo');
+  
+  /* Solution */
+  SELECT name, LENGTH (name) AS length FROM people ORDER BY length DESC;
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE people; --
