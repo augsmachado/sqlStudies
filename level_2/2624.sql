7ea6d2f177510356b9dab14a51020234c5f2f78e
Augs Machado <augustomachadoramos@gmail.com>
2019-08-05 08:44:22 -0300

    Update 2624.sql

--- level_2/2624.sql
+++ level_2/2624.sql
@@ -8,3 +8,28 @@
 		The company board asked you for a simple report on how many cities the company has already reached.
 		To do this you must display the number of distinct cities in the customers table.
 */
+
+CREATE TABLE customers (
+  id numeric PRIMARY KEY,
+  name varchar(255),
+  street varchar(255),
+  city varchar(255),
+  state char(2),
+  credit_limit numeric
+);
+
+
+INSERT INTO customers (id, name, street, city, state, credit_limit)
+VALUES
+  (1,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (2,	'Cecília Olivia Rodrigues', 'Rua Sizuka Usuy',	'Cianorte',	'PR',	3170),
+  (3,	'Augusto Fernando Carlos Eduardo Cardoso',	'Rua Baldomiro Koerich', 'Palhoça',	'SC',	1067),
+  (4,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (5,	'Sabrina Heloisa Gabriela Barros',	'Rua Engenheiro Tito Marques Fernandes',	'Porto Alegre',	'RS',	4312),
+  (6,	'Joaquim Diego Lorenzo Araújo',	'Rua Vitorino',	'Novo Hamburgo',	'RS',	2314);
+  
+  /* Solution */
+  SELECT COUNT(DISTINCT city) as count FROM customers;
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE customers; -- 
