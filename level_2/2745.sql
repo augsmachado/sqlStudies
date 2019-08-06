4c746e21a3cbe57c06c0d74691ba3f72ff7aee9b
Augs Machado <augustomachadoramos@gmail.com>
2019-08-05 22:57:47 -0300

    Update 2745.sql

--- level_2/2745.sql
+++ level_2/2745.sql
@@ -10,3 +10,26 @@
 			which is 10% of his/her income.
 		Show the name and the tax value of each person who earns more than 3000, with two decimal places of precision.
 */
+
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY,
+  name CHARACTER VARYING (255),
+  salary NUMERIC
+);
+
+
+INSERT INTO people(id, name, salary)
+VALUES 
+      (1, 'James M. Tabarez', 883),
+      (2, 'Rafael T. Hendon', 4281),
+      (3, 'Linda J. Gardner', 4437),
+      (4, 'Nicholas J. Conn', 8011),
+      (5, 'Karol A. Morales', 2508),
+      (6, 'Lolita S. Graves', 8709);
+
+  /* Solution */
+  SELECT name, ROUND((salary*0.1), 2) AS tax FROM people
+  WHERE salary > 3000;
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE people; --
