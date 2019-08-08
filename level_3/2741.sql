a656d10de06f3b6b8c398796b0cab19fb2c9eeb2
Augs Machado <augustomachadoramos@gmail.com>
2019-08-08 14:51:57 -0300

    Update 2741.sql

--- level_3/2741.sql
+++ level_3/2741.sql
@@ -11,3 +11,28 @@
 			for those who have been approved (grade ≥7).
 		Remember to sort the list by grade (higher grades first).
 */
+
+CREATE TABLE students(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  grade NUMERIC
+);
+
+
+INSERT INTO students(id, name, grade)
+VALUES (1, 'Terry B. Padilla',7.3),
+       (2, 'William S. Ray',0.6),
+       (3, 'Barbara A. Gongora',5.2),
+       (4, 'Julie B. Manzer', 6.7),
+       (5, 'Teresa J. Axtell', 4.6),
+       (6, 'Ben M. Dantzler', 9.6);
+
+  
+  /* Solution */
+  SELECT CONCAT('Approved: ', name) AS name, grade
+  FROM students
+  WHERE grade >= 7
+  ORDER BY grade DESC;
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE students; --
