5da603d946e0c4496b127becddc045fcbd4dddc0
Augs Machado <augustomachadoramos@gmail.com>
2019-08-15 16:26:28 -0300

    Update 2740.sql

--- level_4/2740.sql
+++ level_4/2740.sql
@@ -11,3 +11,41 @@
 		Select the three first placed with the initial phrase "Podium: " and select the last two, which will be
 			demoted to a lower league with the initial phrase “Demoted:".
 */
+
+CREATE TABLE league(
+  position INTEGER,
+  team CHARACTER VARYING (255)
+);
+
+
+INSERT INTO league(position, team)
+VALUES (1, 'The Quack Bats'),
+       (2, 'The Responsible Hornets'),
+       (3, 'The Bawdy Dolphins'),
+       (4, 'The Abstracted Sharks'),
+       (5, 'The Nervous Zebras'),
+       (6, 'The Oafish Owls'),
+       (7, 'The Unequaled Bison'),
+       (8, 'The Keen Kangaroos'),
+       (9, 'The Left Nightingales'),
+       (10, 'The Terrific Elks'),
+       (11, 'The Lumpy Frogs'),
+       (12, 'The Swift Buffalo'),
+       (13, 'The Big Chargers'),
+       (14, 'The Rough Robins'),
+       (15, 'The Silver Crocs');
+
+  /* Solution */
+  (SELESCT 'Podium: ' || league.team AS name
+   	FROM league
+  	LIMIT 3)
+  UNION ALL
+  (SELECT * FROM (
+	  SELECT 'Demoted: ' || team AS name
+	  FROM league
+	  ORDER BY position DESC
+	  LIMIT 2)
+   demoted ORDER BY 1);
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE league; --
