138f57fe0674cb70e3f6b627613dc10ec05b8d9f
Augs Machado <augustomachadoramos@gmail.com>
2019-08-15 09:31:05 -0300

    Update 2609.sql

--- level_6/2609.sql
+++ level_6/2609.sql
@@ -8,3 +8,42 @@
 		As usual the sales industry is doing an analysis of how many products we have in stock, and you can help them.
 		Then your job will display the name and amount of products of each category.
 */
+
+CREATE TABLE categories (
+  id numeric PRIMARY KEY,
+  name varchar
+);
+
+CREATE TABLE products (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  amount numeric,
+  price numeric(7,2),
+  id_categories numeric REFERENCES categories (id)
+);
+
+INSERT INTO categories (id, name)
+VALUES 
+  (1,	'wood'),
+  (2,	'luxury'),
+  (3,	'vintage'),
+  (4,	'modern'),
+  (5,	'super luxury');
+  
+INSERT INTO products (id, name, amount, price, id_categories)
+VALUES 
+  (1,	'Two-doors wardrobe', 100,	800,	1),
+  (2,	'Dining table',	1000,	560,	3),
+  (3,	'Towel holder',	10000,	25.50,	4),
+  (4,	'Computer desk',	350,	320.50,	2),
+  (5,	'Chair',	3000,	210.64,	4),
+  (6,	'Single bed',	750,	460,	1);
+  
+  /* Solution */
+  SELECT categories.name, sum(products.amount) AS sum
+  FROM categories
+  INNER JOIN products ON categories.id = products.id_categories
+  GROUP BY categories.name;
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE categories,products; --
